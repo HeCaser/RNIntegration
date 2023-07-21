fb2c6b68dd9f46aeff00f43817f4004e73459494
hepan <hepan@xueqiu.com>
2023-07-21 17:29:55 +0800

    feat: homepage

---
+++ pages/homepage/index.js
@@ -0,0 +1,11 @@
+import React, { useEffect} from 'react';
+import { AppRegistry, Button, StyleSheet, Text, View } from 'react-native';
+
+export default HomePage = () => {
+   
+    return (
+        <Text>我是一个 RN 页面</Text>
+    )
+}
+
+
