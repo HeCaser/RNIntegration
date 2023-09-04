8aca8091d35030f187f9623584eee250e83940a6
hepan <hepan@xueqiu.com>
2023-09-04 17:52:07 +0800

    feat: 获取 view 宽度

--- pages/SVG/component/line.js
+++ pages/SVG/component/line.js
@@ -1,14 +1,20 @@
 import Svg, { Line } from 'react-native-svg';
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 
 function LineDemo() {
 
-    const a = 30
+    const [width, setWidth] = useState(0)
+
+    const onLayout = (event) => {
+        const { x, y, height, width } = event.nativeEvent.layout;
+        setWidth(width)
+    }
+
     return (
-        <View width={"100%"} height={60}>
+        <View width={"100%"} height={60} onLayout={onLayout}>
             <Svg height="90%" width="100%" style={{ backgroundColor: '#0ff' }}>
-                <Line x1={0} y1={a} x2={50} y2={25}  strokeWidth={0.5}  stroke="red" > </Line>
+                <Line x1={0} y1={25} x2={width} y2={25} strokeWidth={0.5} stroke="red" > </Line>
             </Svg>
         </View>
     )
