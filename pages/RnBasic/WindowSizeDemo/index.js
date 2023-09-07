aa5b2d3bfa30de74f28f318a1a734da3c2416c4e
hepan <hepan@xueqiu.com>
2023-09-07 20:19:30 +0800

    feat: build release bundle

--- pages/RnBasic/WindowSizeDemo/index.js
+++ pages/RnBasic/WindowSizeDemo/index.js
@@ -1,11 +1,15 @@
 import React from "react";
-import {Dimensions,View,Text} from 'react-native';
+import {Dimensions,View,Text,useWindowDimensions} from 'react-native';
 
 function WindowSizeDemo() {
     const windowWidth = Dimensions.get('window').width;
+
+    const {height, width} = useWindowDimensions();
+
     return(
         <View>
             <Text> width = {windowWidth}</Text>
+            <Text> width2 = {width}</Text>
         </View>
     )
 
