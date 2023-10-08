3df41df0b597a67d088e0f4215e2fb4d4c74310b
hepan <hepan@xueqiu.com>
2023-10-08 16:29:29 +0800

    feat: Txt 支持 style

--- pages/SnowBox/Txt/index.tsx
+++ pages/SnowBox/Txt/index.tsx
@@ -5,12 +5,12 @@ import { TxtTypes, fontProps } from './props';
 /***
  * Txt 是对 Text 的封装, 
  */
-const Txt = ({ children, ...props }: TxtTypes) => {
+const Txt = ({ style, children, ...props }: TxtTypes) => {
 
     const newProps = { ...props };
 
     let fontStylesObj: TextStyle = {
-        fontSize:16
+        fontSize: 16
     };
 
     Object.keys(newProps).map((item) => {
@@ -21,7 +21,7 @@ const Txt = ({ children, ...props }: TxtTypes) => {
                 fontStylesObj[fontProps[item].property] = newProps[item]
             }
             // 转换 f 属性为 fontSize
-            if(item === 'f' && newProps.f){
+            if (item === 'f' && newProps.f) {
                 fontStylesObj[fontProps[item].property] = newProps[item]
             }
 
@@ -32,7 +32,7 @@ const Txt = ({ children, ...props }: TxtTypes) => {
 
 
     return (
-        <Text style={[fontStylesObj]}>{children}</Text>
+        <Text style={[fontStylesObj, style]}>{children}</Text>
     )
 }
 
