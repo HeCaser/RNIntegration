568aa9c4bdc6e920993e0aeb5f9ffae4b1f02c40
hepan <hepan@xueqiu.com>
2023-10-08 18:21:02 +0800

    feat: readme.md

--- pages/SnowBox/README.md
+++ pages/SnowBox/README.md
@@ -2,3 +2,7 @@ snowbox 学习
 
 [官网链接](https://snowbox.js.org)
 
+## Txt
+- 参数传递
+- 参数简化 -> 转换 style
+
--- pages/SnowBox/index.tsx
+++ pages/SnowBox/index.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, TextStyle, ScrollView, Button } from 'react-native';
+import { StyleSheet, Text, TextStyle, ScrollView, Button, View } from 'react-native';
 import Txt from './Txt';
 
 const SnowBoxDemo = () => {
-
-    const hh = () => {
-        alert(19)
-    }
     return (<ScrollView>
         <Txt cl='#f00' f={14} >我是 Txt</Txt>
         <Txt cl='#00f' f={20} style={{backgroundColor:'#0ff'}}>我是 MarkDown</Txt>
-        <Button onPress={hh} title='点击我' style={{transform: [{ rotate: '45deg' }] ,}}></Button>
+        <View style={{width:10,height:10,backgroundColor:'#00f',transform:[{rotate:'45deg'}]}}></View>
     </ScrollView>)
 }
 
