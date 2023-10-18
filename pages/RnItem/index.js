1708aaddf2f59b8e7aa3b17619851531c34972fd
hepan <hepan@xueqiu.com>
2023-10-18 18:18:47 +0800

    feat: code reset

--- pages/RnItem/index.js
+++ pages/RnItem/index.js
@@ -3,7 +3,7 @@ import { AppRegistry, Button, StyleSheet, Text, View, Image } from 'react-native
 
 const logmsg = 'hepan lifecycle  '
 
-function RnItem1(props) {
+function RnItem(props) {
 
     const a = useRef(0)
     const [isAddWeChat, setIsAddWeChat] = useState(true)
@@ -32,30 +32,30 @@ function RnItem1(props) {
 }
 
 // class 写法的 RnItem, 验证生命周期
-class RnItem extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            param: props.param,
-            show_child: true
-        };
-        console.log(logmsg + "constructor() ")
-    }
+// class RnItem extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             param: props.param,
+//             show_child: true
+//         };
+//         console.log(logmsg + "constructor() ")
+//     }
 
-    render() {
-        console.log(logmsg + "render() ")
-        return (
-            <View>
-                <Text style={{ color: '#007AFF' }}>RnItem Class </Text>
-                <Text>Native 传递的参数 param = {`${this.props.param}`}</Text>
+//     render() {
+//         console.log(logmsg + "render() ")
+//         return (
+//             <View>
+//                 <Text style={{ color: '#007AFF' }}>RnItem Class </Text>
+//                 <Text>Native 传递的参数 param = {`${this.props.param}`}</Text>
 
-                <Image source={require('./fund_hot.png')} style={{ width: '100%', height: 120 }} />
-                <Button onPress={() => { this.setState({ show_child: !this.state.show_child }) }} title={`点击改变 child = ${this.state.show_child}`} />
-            </View>
-        );
-    }
+//                 <Image source={require('./fund_hot.png')} style={{ width: '100%', height: 120 }} />
+//                 <Button onPress={() => { this.setState({ show_child: !this.state.show_child }) }} title={`点击改变 child = ${this.state.show_child}`} />
+//             </View>
+//         );
+//     }
 
-}
+// }
 export default RnItem
 
 
