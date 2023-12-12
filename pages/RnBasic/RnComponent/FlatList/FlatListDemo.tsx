25c7531fd6f30598e6ebbcca49050535fb06691d
hepan <hepan@xueqiu.com>
2023-12-12 18:37:59 +0800

    feat: show iamge

--- pages/RnBasic/RnComponent/FlatList/FlatListDemo.tsx
+++ pages/RnBasic/RnComponent/FlatList/FlatListDemo.tsx
@@ -15,7 +15,15 @@ import { USERS, User, createRandomUser } from "./interface";
 
 const styles = StyleSheet.create({
     item: {
-        padding: 16
+        padding: 16,
+        flexDirection: 'row'
+    },
+    avator: {
+        width: 40,
+        height: 40,
+        borderRadius: 60,
+        alignSelf: 'center',
+        backgroundColor:'red'
     },
     button: {
         backgroundColor: 'blue',
@@ -40,19 +48,25 @@ export const FlatListDemo = () => {
 
     const renderItem = (user: User, add: () => void) => {
         return <View style={styles.item}>
-            <Text>姓名: {user.username}</Text>
-            <Text>邮箱: {user.email}</Text>
-            <View style={{ flexDirection: 'row' }}>
-                <Text style={{ alignSelf: 'center' }}>Star: {user.star}</Text>
-                <TouchableOpacity style={{ alignSelf: 'center', marginLeft: 10 }} onPress={add}>
-                    <Text style={styles.button}>增加</Text>
-                </TouchableOpacity>
+
+            <Image source={{ uri: user.avatar }} style={styles.avator} />
+
+            <View style={{ marginLeft: 12, flex:1}}>
+                <Text>姓名: {user.username}</Text>
+                <Text>邮箱: {user.email}</Text>
+                <View style={{ flexDirection: 'row' }}>
+                    <Text style={{ alignSelf: 'center' }}>Star: {user.star}</Text>
+                    <TouchableOpacity style={{ alignSelf: 'center', marginLeft: 10 }} onPress={add}>
+                        <Text style={styles.button}>增加</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
+
 
         </View>
     }
 
-    const addStar = (item:User) => {
+    const addStar = (item: User) => {
         let newList = user.map(it => it.userId === item.userId ? { ...it, star: item.star!! + 1 } : { ...it })
         setUser(newList)
     }
@@ -61,18 +75,19 @@ export const FlatListDemo = () => {
         flastList.current?.scrollToEnd()
     }
     const top = () => {
-        flastList.current?.scrollToIndex({index:0})
+        flastList.current?.scrollToIndex({ index: 0 })
     }
 
 
     return <View style={{ flex: 1 }}>
-        <View style={{flexDirection:'row', justifyContent:'space-around'}}>
-        <Button onPress={addUser} title="添加"></Button>
-        <Button onPress={end} title="End" ></Button>
-        <Button onPress={top} title="Top" ></Button>
+      
+        <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+            <Button onPress={addUser} title="添加"></Button>
+            <Button onPress={end} title="End" ></Button>
+            <Button onPress={top} title="Top" ></Button>
 
         </View>
-        <FlatList ref={flastList} data={user} renderItem={({ item, index }) => renderItem(item, ()=>{addStar(item)})} keyExtractor={item => item.userId} >
+        <FlatList ref={flastList} data={user} renderItem={({ item, index }) => renderItem(item, () => { addStar(item) })} keyExtractor={item => item.userId} >
 
         </FlatList>
 
