a2f4da36bbda30e340f3e520d1ceeb110e32a1f8
hepan <hepan@xueqiu.com>
2023-12-26 09:40:03 +0800

    feat: flex and width=auto

--- pages/RnBasic/RnComponent/Flex/index.tsx
+++ pages/RnBasic/RnComponent/Flex/index.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react"
-import { ScrollView, View, Text } from "react-native"
+import { ScrollView, View, Text, StyleSheet } from "react-native"
 
 export const FlexDemo = () => {
     return (<ScrollView>
-        <View style={{ flex: 1, backgroundColor: 'gray' }}>
+        <View style={{ flex: 1, backgroundColor: 'aliceblue' }}>
 
-            <Text>Flex 使用</Text>
+            <Text>Flex 使用. 详细用法见官网 https://reactnative.dev/docs/flexbox </Text>
             <Demo1></Demo1>
+            <Demo2></Demo2>
 
         </View>
     </ScrollView>)
@@ -22,3 +23,30 @@ const Demo1 = () => {
         </View>
     </View>
 }
+
+
+const Demo2 = () => {
+    return <View>
+        <Text>2. width = 'auto' 自适应宽度, 剩余空间按需分配 </Text>
+        <View style={{ flex: 1, flexDirection: 'row', height: 30 }}>
+            <View style={{ flex: 1, backgroundColor: 'red' }} />
+            <View style={{ flex: 2, backgroundColor: 'darkorange' }} />
+            <View style={{ flex:3, backgroundColor: 'green' }} />
+            <Text style={[styles.box, { width: 'auto', backgroundColor: 'oldlace' }]}>自适应宽度</Text>
+        </View>
+    </View>
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 8,
+        backgroundColor: 'aliceblue',
+        minHeight: 200,
+    },
+    box: {
+        width: 50,
+        height: 50,
+    }
+})
+
